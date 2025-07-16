9f7e38e4017832b4feafc166fdb9cc50d893b55d
0xbea <19748276+0xbea@users.noreply.github.com>
2025-07-16 20:01:46 +0200

    put player in containers

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = (updatable, drawable)
     newplayer = player.Player(
         x=constants.SCREEN_WIDTH / 2, y=constants.SCREEN_HEIGHT / 2
     )
@@ -18,9 +21,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        newplayer.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        newplayer.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         # Limit the frame rate to 60 FPS
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
